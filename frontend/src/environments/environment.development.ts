1c8647f5656a6d5bf061f1ccbd07fdfe860b8026
Nrupathunga A V <nrupathunga.cs24@bmsce.ac.in>
2026-01-01 11:44:18 +0530

    Deployment: Final configurations for Render (Environments and CORS)

---
+++ frontend/src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000/api',
+  firebase: {
+    apiKey: "AIzaSyCzt9aAR-o0Bmwg2c8MSuRJCCw4i-Cz7pE",
+    authDomain: "bookswap-b7eae.firebaseapp.com",
+    projectId: "bookswap-b7eae",
+    storageBucket: "bookswap-b7eae.firebasestorage.app",
+    messagingSenderId: "611986670924",
+    appId: "1:611986670924:web:91ab058f018411029afbe9",
+    measurementId: "G-EFQ73VQP66"
+  }
+};
